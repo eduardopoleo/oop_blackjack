744f2eb5de5cc22179f3a12f0efe43f985e3e849
eduardopoleo <eduardopoleo@gmail.com>
2014-09-03 00:52:27 -0400

    Multiple players implementation

--- oo_blackjack.rb
+++ oo_blackjack.rb
@@ -5,6 +5,10 @@ class Player
 		@hand = Hand.new
 		@name = name
 	end
+
+	def to_s
+		"hey I'm #{name}"
+	end
 	
 end
 
@@ -65,102 +69,152 @@ class Deck
 end
 
 class Game
-	attr_accessor :player, :dealer, :deck
+	attr_accessor :players, :dealer, :deck, :no_players
 
 	def initialize
 		system("clear")
+		@players = []
+
+		begin
+			puts "How many players are playing today (1-5)"
+			@no_players = gets.chomp.to_i
+		end until (1..5).include?(no_players)
+		
+		no_players.times do |player_number|
+			player_name = "Player_" + (player_number + 1).to_s
+			players[player_number] = Player.new(player_name)
+		end
+
+		players[no_players] = Player.new("Dealer")
 		@deck = Deck.new
-		@player = Player.new("Player_1")
-		@dealer = Player.new("El macho")
 	end
 	
 	def initial_deal
-		2.times{player.hand.get_card(deck.cards)}
-		2.times{dealer.hand.get_card(deck.cards)}
-
-		blackJack_or_busted?(player.name, player.hand)
+		dealer_plays = true
+		(no_players + 1).times do |number|
+			puts number
+			2.times{players[number].hand.get_card(deck.cards)}
+			blackJack_or_busted?(players[number].name, players[number].hand)
+		end
+		display(dealer_plays)
+		all_players_blackjack_or_busted?
 	end
 
-	def player_turn
+	def players_turn
 		dealer_plays = false
 		display(dealer_plays)
-		begin
-			puts " "
-			puts "Do you want to hit or stay? (h/s)"
 
-			choice = gets.chomp.downcase
-			if choice == "h"
-				player.hand.get_card(deck.cards)
-				system("clear")
-				display(dealer_plays)
-			end 
+		no_players.times do |number|
+			if players[number].hand.value == 21
+				puts "#{players[number].name} you got BlackJack! why would you want another card?"
+				next
+			end
 
-			if (player.hand.value >= 21) then break end
-		end until choice == "s"
-		blackJack_or_busted?(player.name, player.hand)
+			begin
+				puts " "
+				puts "#{players[number].name}'s Turn"
+				puts "Do you want to hit or stay? (h/s)"
+
+				choice = gets.chomp.downcase
+				if choice == "h"
+					players[number].hand.get_card(deck.cards)
+					system("clear")
+					display(dealer_plays)
+				end 
+				break if players[number].hand.value >= 21 
+			end until choice == "s"
+
+			blackJack_or_busted?(players[number].name, players[number].hand)
+		end
+		all_players_blackjack_or_busted?
 	end
 
 	def dealer_turn
 		dealer_plays = true
-		while dealer.hand.value < 17 
-			dealer.hand.get_card(deck.cards)
-		end
 		display(dealer_plays)
-		blackJack_or_busted?(dealer.name, dealer.hand)
+		blackJack_or_busted?(players[no_players].name, players[no_players].hand)
+
+		while players[no_players].hand.value < 17 
+			players[no_players].hand.get_card(deck.cards)
+		end
+
+		display(dealer_plays)
+		blackJack_or_busted?(players[no_players].name, players[no_players].hand)
 	end
 
 	def blackJack_or_busted?(player_name, player_hand)
-			if player_hand.value == 21 && player_hand.cards == 2
-				puts "BlackJack!, #{player_name} won!"
-				play_again?
-			elsif player_hand.busted?
-				puts "#{player_name} busted! and lost"
-				play_again?
-			end
+		if player_hand.value == 21 && player_hand.cards.size == 2
+			puts "BlackJack!, #{player_name} won!"
+			play_again? if player_name == "Dealer"
+		elsif player_hand.busted?
+			puts "#{player_name} busted! and lost"
+			play_again? if player_name == "Dealer"
+		end
 	end
 
-	def compare_hands(player_hand,dealer_hand)
-		if dealer.hand.value == 21
-			puts "The dealer kicked your ass and won"
-		elsif dealer.hand.value >= player.hand.value
-			puts "Mr. D is better than you. You loose!"
-		else
-			puts "You won!"
+	def all_players_blackjack_or_busted?
+		if players.select{|player| player.hand.value > 21}.size == no_players
+			puts "Wao these guys are a bunch of whimps!"
+			play_again?
+		elsif (players.select{|player| player.hand.value == 21}.size == no_players) && 
+			(players.select{|player| player.hand.cards.size == 2}.size == no_players)
+			puts "Everyone gets BlackJack!"
+			play_again?
 		end
+	end
+
+	def compare_hands
+		puts "Summary: "
+		no_players.times do |number|
+			puts " "
+			if players[number].hand.value > 21
+				puts "#{players[number].name} busted"
+			elsif players[number].hand.value == 21 && players[number].hand.cards.size == 2
+				puts "#{players[number].name} got BlackJack!"
+			elsif players[number].hand.value <= players[no_players].hand.value
+				puts "#{players[number].name} Mr.D beat you"
+			else
+				puts "#{players[number].name} You won!"
+		end
+	end
 		play_again?
 	end
 
 	def display(dealer_plays)
 		system("clear")
 		puts "Welcome to my awesome BlackJack Game"
-		puts " "
-		puts "---------#{player.name}'s hand----------"	
-		puts " "
-		player.hand.cards.each do |card|
-			puts "#{card[0]} of #{card[1]}"
+
+		no_players.times do |number|
+			puts " "
+			puts "---------#{players[number].name}'s hand----------"	
+			puts " "
+			players[number].hand.cards.each do |card|
+				puts "#{card[0]} of #{card[1]}"
+			end
+			puts " "
+			puts "Accumulated Score: #{players[number].hand.value}"
+			puts " "
 		end
-		puts " "
-		puts "Accumulated Score: #{player.hand.value}"
-		puts " "
-		puts "---------#{dealer.name}'s hand----------"
+		puts "---------#{players[no_players].name}'s hand----------"
 		puts " "
 		if dealer_plays
-			dealer.hand.cards.each do |card|
+			players[no_players].hand.cards.each do |card|
 			puts "#{card[0]} of #{card[1]}"
 			end
 			puts " "
-			puts "Accumulated Score: #{dealer.hand.value}"
+			puts "Accumulated Score: #{players[no_players].hand.value}"
 		else
 			puts "First card is hidden. The second card is: "
 			puts " "
-			puts "#{dealer.hand.cards[1][0]} of #{dealer.hand.cards[1][1]}"
+			puts "#{players[no_players].hand.cards[1][0]} of #{players[no_players].hand.cards[1][1]}"
 		end
 		puts "----------------------------------------"
 	end
 
 	def reset_hands
-		player.hand.reset
-		dealer.hand.reset
+		(no_players + 1).times do |number|
+			players[number].hand.reset
+		end
 	end	
 
 	def play_again?
@@ -181,9 +235,9 @@ class Game
 	def play
 		reset_hands
 		initial_deal
-		player_turn
+		players_turn
 		dealer_turn
-		compare_hands(player.hand,dealer.hand)
+		compare_hands
 	end
 end
 
